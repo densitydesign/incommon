2b4fc05bb45c34499cbf047964a1b783af805dd3
skaffo <matteo.scaffidi@gmail.com>
2020-06-22 11:32:48 +0200

    Insert spettacoli static to page spettacoli

--- src/pages/Spettacolo/Spettacolo.js
+++ src/pages/Spettacolo/Spettacolo.js
@@ -1,10 +1,48 @@
 import React from 'react'
 import MenuTop from '../../components/MenuTop'
+import './Spettacolo.css'
 
 export default function Spettacolo(){
   return (
     <div className='Spettacolo'>
       <MenuTop />
-    </div>
+        <div className='container-fluid'>
+          <div className='row mt-3'>
+            <div className='col-md-4'>
+              <div className='item-spettacolo' style={{ backgroundImage: 'url(/spettacolo/A-charlie-parker.png)'}}>
+                <div className='info-documento d-flex justify-content-between'>
+                  <div className='num_documenti'>100 doc.</div>
+                  <div className='data_documenti'>1970</div>
+                </div>
+                <div className='titolo-spettacolo'>
+                  A Charlie<br />Parker
+                </div>
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <div className='item-spettacolo' style={{ backgroundImage: 'url(/spettacolo/Aspettando-godot.png)'}}>
+                <div className='info-documento d-flex justify-content-between'>
+                  <div className='num_documenti'>100 doc.</div>
+                  <div className='data_documenti'>1965</div>
+                </div>
+                <div className='titolo-spettacolo'>
+                  Aspettando<br />Godot
+                </div>
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <div className='item-spettacolo' style={{ backgroundImage: 'url(/spettacolo/Atto-senza-parole-II.png)'}}>
+                <div className='info-documento d-flex justify-content-between'>
+                  <div className='num_documenti'>100 doc.</div>
+                  <div className='data_documenti'>1965</div>
+                </div>
+                <div className='titolo-spettacolo'>
+                  Atto senza<br />Parole II
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
   )
 }
