1b13e1a5a138103db7bb90c55ad88dbaf81eccf5
gffuma <fumagalli.gf@gmail.com>
2020-09-21 09:41:09 +0200

    Slideshow avoid using refs

--- src/pages/Slideshow/Slideshow.js
+++ src/pages/Slideshow/Slideshow.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useImperativeHandle, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import './Slideshow.css'
 import MenuTop from '../../components/MenuTop/MenuTop'
 import slideshowConfig from './slideshow.json'
@@ -8,62 +8,38 @@ const totalImages = slideshowConfig.reduce(
   0
 )
 
-function AnimatedImageBase({ style, animatedStyle, time, src }, ref) {
-  const [entered, setEntered] = useState(false)
-
+function AnimatedImageBase({ style, animatedStyle, time, src, entered }) {
   let imageStyle = style
   if (entered && animatedStyle) {
     imageStyle = { ...imageStyle, ...animatedStyle }
   }
 
-  useImperativeHandle(ref, () => ({
-    enter: () => {
-      setEntered(true)
-    },
-    exit: () => {
-      setEntered(false)
-    },
-  }))
-
   return <img style={imageStyle} src={src} alt="Slide" />
 }
 
-const AnimatedImage = React.forwardRef(AnimatedImageBase)
+const AnimatedImage = React.memo(AnimatedImageBase)
 
 export default function Slideshow() {
-  const [currentIndex, setIndex] = useState(0)
+  const [index, setIndex] = useState(0)
+
   function goPrev() {
-    let index = currentIndex
-    if (index > 0) {
-      const image = imagesRef.current[index].current
-      image.exit()
-      index = index - 1
-      setIndex(index)
-    }
+    setIndex((index) => {
+      if (index > 0) {
+        return index - 1
+      }
+      return index
+    })
   }
   function goNext() {
-    let index = currentIndex
-    if (index < totalImages - 1) {
-      index = index + 1
-      const image = imagesRef.current[index].current
-      image.enter()
-      setIndex(index)
-    }
+    setIndex((index) => {
+      if (index < totalImages - 1) {
+        return index + 1
+      }
+      return index
+    })
   }
 
   let z = 0
-  const imagesRef = useRef({})
-  function imageRef(i) {
-    if (!imagesRef.current[i]) {
-      imagesRef.current[i] = React.createRef()
-    }
-    return imagesRef.current[i]
-  }
-
-  useEffect(() => {
-    const image = imagesRef.current[0].current
-    image.enter()
-  }, [])
 
   function handleKeyDown(e) {
     if (e.key === 'ArrowRight') {
@@ -81,14 +57,16 @@ export default function Slideshow() {
           <div key={i} style={container.style}>
             {container.images.map((image, j) => {
               z++
-              return <AnimatedImage ref={imageRef(z - 1)} key={j} {...image} />
+              return (
+                <AnimatedImage entered={index >= z - 1} key={j} {...image} />
+              )
             })}
           </div>
         ))}
         <div onClick={goPrev} className="slide-click-spy-prev" />
         <div onClick={goNext} className="slide-click-spy-next" />
         <div className="slide-index">
-          {currentIndex + 1} / {totalImages}
+          {index + 1} / {totalImages}
         </div>
       </div>
     </div>
