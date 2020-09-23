07c5b1d9a580349a0ca4499beda239703f7a7587
skaffo <matteo.scaffidi@gmail.com>
2020-09-23 15:35:08 +0200

    Dettaglio spettacolo page

--- src/pages/DettaglioSpettacolo/DettaglioSpettacolo.js
+++ src/pages/DettaglioSpettacolo/DettaglioSpettacolo.js
@@ -1,41 +1,43 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import MenuTop from '../../components/MenuTop'
-import InfoSpettacolo from '../../components/InfoSpettacolo'
-import FiltersSpettacoloDetail from '../../components/FiltersSpettacoloDetail'
-import './DettaglioSpettacolo.css'
-import slideshowConfig from '../Slideshow/slideshow.json'
+import React, { useState } from "react"
+import MenuTop from "../../components/MenuTop"
+import { useHistory } from 'react-router-dom'
+import InfoSpettacolo from "../../components/InfoSpettacolo"
+import FiltersSpettacoloDetail from "../../components/FiltersSpettacoloDetail"
+import "./DettaglioSpettacolo.css"
+import slideshowConfig from "../Slideshow/slideshow.json"
 
-
-export default function DettaglioSpettacolo(){
-
-  const [showMoreInfo,setShowMoreInfo] = useState(false)
+export default function DettaglioSpettacolo() {
+  const [showMoreInfo, setShowMoreInfo] = useState(false)
 
   const toggleShowMoreInfo = () => {
     setShowMoreInfo(!showMoreInfo)
   }
 
-  console.log({ tranform: 'rotate('+Math.floor(Math.random() * 10)+')'})
+  const history = useHistory()
 
   return (
-    <div className='DettaglioSpettacolo'>
+    <div className="DettaglioSpettacolo">
       <MenuTop />
-      <div className='d-flex'>
-        {showMoreInfo &&
+      <div className="d-flex">
+        {showMoreInfo && (
           <FiltersSpettacoloDetail toggleShowMoreInfo={toggleShowMoreInfo} />
-        }
+        )}
         <InfoSpettacolo toggleShowMoreInfo={toggleShowMoreInfo} />
-        <div className='body-spettacolo d-flex align-items-center'>
-          {slideshowConfig.map(image => (
+        <div className="body-spettacolo d-flex align-items-center">
+          {slideshowConfig.map((image) =>
             image.images.map((img, key) => {
-              const degRotation = Math.floor(Math.random() * 30) + 1
-              const translate = Math.floor(Math.random() * 6) + 1
               return (
-              <div style={{ position: 'absolute', left: '40%', top: '15%', transform:'rotate('+degRotation+'deg)'}} key={key}>
-                <img src={img.src} alt='Img' width='450' />
-              </div>
-            )})
-          ))}
+                <div
+                  style={img.style_list}
+                  key={key}
+                  className='pointer'
+                  onClick={() => history.push('/slideshow')}
+                >
+                  <img src={img.src} alt="Img" width="450" />
+                </div>
+              )
+            })
+          )}
         </div>
       </div>
     </div>
