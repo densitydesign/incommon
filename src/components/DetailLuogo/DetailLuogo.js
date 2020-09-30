9bd82e57eb77b60870c41682e5ef4573818d4303
skaffo <matteo.scaffidi@gmail.com>
2020-09-30 14:10:15 +0200

    Adding name town

--- src/components/DetailLuogo/DetailLuogo.js
+++ src/components/DetailLuogo/DetailLuogo.js
@@ -2,13 +2,14 @@ import React from "react"
 import "./DetailLuogo.css"
 
 export default function DetailLuogo({ town, toggleTown }) {
+
   return (
     <div className="DetailLuogo">
       <div className="pointer p-4 text-right" onClick={() => toggleTown(null)}>
         <img src="/close-document.svg" alt="close" />
       </div>
       <div className='p-4'>
-        <h4>{town.name}</h4>
+        <h4>{town.citta}</h4>
       </div>
       
     </div>
