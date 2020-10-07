d116a28d2ec0996365b271bdb1a43c7f1e16c739
skaffo <matteo.scaffidi@gmail.com>
2020-10-07 13:45:48 +0200

    Adding component document

--- src/pages/Catalogo/Catalogo.js
+++ src/pages/Catalogo/Catalogo.js
@@ -1,52 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import MenuTop from '../../components/MenuTop'
-import FiltersCatalogo from '../../components/FiltersCatalogo'
-import preval from 'preval.macro'
-import './Catalogo.css'
-import { useDocuments, useDocumentsCount } from '../../hooks/documents'
+import React, { useCallback, useState } from "react"
+import { Link } from "react-router-dom"
+import MenuTop from "../../components/MenuTop"
+import FiltersCatalogo from "../../components/FiltersCatalogo"
+import Document from "../../components/Document"
+import "./Catalogo.css"
+import { useDocuments, useDocumentsCount } from "../../hooks/documents"
 
 const params = {
   page: 1,
 }
+
 export default function Catalogo() {
-
   const [{ documents, hasNext, count }] = useDocuments(params)
-  console.log(documents, hasNext, count)
-
   const [{ countInfo }] = useDocumentsCount()
-  console.log(countInfo)
 
-  const images = preval`
-    const fs = require('fs')
-    const files = fs.readdirSync(__dirname+'/../../../public/catalogo')
-    module.exports = files
-  `
+  const [isCollapsed, setCollapsed] = useState(false)
+
+  const toggleCollapseDocuments = useCallback(() => {
+    setCollapsed(collapse => !collapse)
+  }, [])
 
   return (
-    <div className='Catalogo'>
+    <div className="Catalogo">
       <MenuTop />
-      <div className='d-flex'>
-        <div className='block-filters'>
-          <div className='d-flex'>
-            <div className='raggruppa-button'>raggruppa i fascicoli</div>
-            <div className='reset-filtri'>cancella i filtri</div>
+      <div className="d-flex">
+        <div className="block-filters">
+          <div className="d-flex">
+            <div className="raggruppa-button" onClick={() => toggleCollapseDocuments()}>raggruppa i fascicoli</div>
+            <div className="reset-filtri">cancella i filtri</div>
           </div>
-          <div style={{ height: 200 }}>
-
+          <div className="count-documents">
+            {count && countInfo && (
+              <>
+                <span className="medium-font font-weight-bold mr-2">
+                  {count} / {countInfo.count}
+                </span>{" "}
+                documenti
+              </>
+            )}
           </div>
-          <div className='container'>
+          <div className="container">
             <FiltersCatalogo />
           </div>
         </div>
-        <div className='block-catalogo ml-4 mr-4 d-flex flex-row flex-wrap'>
-          {images && images.map((image,key) => (
-            <div style={{ height: 120 }} key={key} className='mr-4 mt-4 pointer'>
-              <Link to={'/catalogo-dei-documenti/1'}>
-                <img height={120} src={`/catalogo/${image}`} alt='Catalogo' />
-              </Link>
-            </div>
-          ))}
+        <div className='block-catalogo ml-4 mr-4 mb-4 d-flex flex-row flex-wrap'>
+          {documents &&
+            documents.map((document, index) => (
+              <Document key={index} document={document} />
+            ))}
         </div>
       </div>
     </div>
