cfcf8db4a2e6ec9179fbe890a8fef6845269b483
skaffo <matteo.scaffidi@gmail.com>
2020-10-09 12:30:15 +0200

    Adding controls carousel to docuemtn detail

--- src/components/DettaglioDocumento/FiltersDocumentDetail/FiltersDocumentDetail.js
+++ src/components/DettaglioDocumento/FiltersDocumentDetail/FiltersDocumentDetail.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import classnames from "classnames"
+import { useHistory } from "react-router-dom"
 import "./FiltersDocumentDetail.css"
 
 export default function FiltersDocumentDetail({ document }) {
@@ -9,7 +10,7 @@ export default function FiltersDocumentDetail({ document }) {
     setShowMoreInfo(!showMoreInfo)
   }
 
-  console.log(document,'document')
+  const history = useHistory()
 
   return (
     <div className="block-left-detail position-relative">
@@ -44,63 +45,129 @@ export default function FiltersDocumentDetail({ document }) {
                   <span className="ml-5 text-red">{document.id}</span>
                 </div>
               )}
-              <div className="mt-2">
+              {/* <div className="mt-2">
                 <span>diritti</span>
                 <span className="ml-5 text-red"></span>
-              </div>
-              <div className="mt-2">
-                <span>referente</span>
-                <span className="ml-5 text-red">Baravalle, M.</span>
-              </div>
-              <div className="mt-3">
+              </div> */}
+              {document.persona && (
+                <div className="mt-2">
+                  <span>referente</span>
+                  <span className="ml-5 text-red"></span>
+                </div>
+              )}
+              {/* <div className="mt-3">
                 informazioni sui diritti del documento
                 <div className="text-red mt-1">
                   Archivio Privato Giuliano Scabia, Firenze - Giuliano Scabia
                 </div>
-              </div>
+              </div> */}
             </div>
           )}
         </div>
       </div>
-      <div className="block-filters-detail overflow-scroll">
+      <div className="block-filters-detail overflow-auto">
         <div className="label-filter">Filtra il catalogo per:</div>
         {document && (
           <>
-            <div className="filter-block mt-3">
-              <div className="filter-name">spettacolo</div>
-              <div className="filter-body">
-                <div className="circle-filter">{document.spettacolo}</div>
+            {document.spettacolo && (
+              <div className="filter-block-document mt-3">
+                <div className="filter-name">spettacolo</div>
+                <div className="filter-body">
+                  <div
+                    onClick={() =>
+                      history.push(
+                        `/catalogo-dei-documenti?spettacolo=${document.spettacolo}`
+                      )
+                    }
+                    className="circle-filter pointer"
+                  >
+                    {document.spettacolo}
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="filter-block">
-              <div className="filter-name">luogo</div>
-              <div className="filter-body">
-                <div className="circle-filter">{document.luogo}</div>
-                {document.citta && (
-                  <div className="circle-filter">{document.citta}</div>
-                )}
+            )}
+            {(document.luogo || document.citta) && (
+              <div className="filter-block-document">
+                <div className="filter-name">luogo</div>
+                <div className="filter-body">
+                  {document.luogo && (
+                    <div
+                      onClick={() =>
+                        history.push(
+                          `/catalogo-dei-documenti?luogo=${document.luogo}`
+                        )
+                      }
+                      className="circle-filter pointer"
+                    >
+                      {document.luogo}
+                    </div>
+                  )}
+                  {document.citta && (
+                    <div
+                      onClick={() =>
+                        history.push(
+                          `/catalogo-dei-documenti?citta=${document.citta}`
+                        )
+                      }
+                      className="circle-filter pointer"
+                    >
+                      {document.citta}
+                    </div>
+                  )}
+                </div>
               </div>
-            </div>
-            <div className="filter-block">
-              <div className="filter-name">data</div>
-              <div className="filter-body">
-                <div className="circle-filter">{document.data}</div>
-                <div className="circle-filter">{document.anno}</div>
+            )}
+            {(document.data || document.anno) && (
+              <div className="filter-block-document">
+                <div className="filter-name">data</div>
+                <div className="filter-body">
+                  <div className="circle-filter">{document.data}</div>
+                  {document.anno && (
+                    <div
+                      onClick={() =>
+                        history.push(
+                          `/catalogo-dei-documenti?anno=${document.anno}`
+                        )
+                      }
+                      className="circle-filter pointer"
+                    >
+                      {document.anno}
+                    </div>
+                  )}
+                </div>
               </div>
-            </div>
+            )}
             {document.tipologia && (
-              <div className="filter-block">
+              <div className="filter-block-document">
                 <div className="filter-name">tipologia</div>
                 <div className="filter-body">
-                  <div className="circle-filter">{document.tipologia}</div>
+                  <div
+                    onClick={() =>
+                      history.push(
+                        `/catalogo-dei-documenti?tipologia=${document.tipologia}`
+                      )
+                    }
+                    className="circle-filter pointer"
+                  >
+                    {document.tipologia}
+                  </div>
                 </div>
               </div>
             )}
             {document.persona && (
-              <div className="filter-block">
+              <div className="filter-block-document">
                 <div className="filter-name">creatore del documento</div>
                 <div className="filter-body">
-                  <div className="circle-filter">{document.persona}</div>
+                  <div
+                    onClick={() =>
+                      history.push(
+                        `/catalogo-dei-documenti?persona=${document.persona}`
+                      )
+                    }
+                    className="circle-filter pointer"
+                  >
+                    {document.persona}
+                  </div>
                 </div>
               </div>
             )}
--- src/pages/DettaglioDocumento/DettaglioDocumento.js
+++ src/pages/DettaglioDocumento/DettaglioDocumento.js
@@ -1,14 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 import MenuTop from "../../components/MenuTop"
 import FiltersDocumentDetail from "../../components/DettaglioDocumento/FiltersDocumentDetail"
 import "./DettaglioDocumento.css"
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch"
+import "react-responsive-carousel/lib/styles/carousel.min.css" // requires a loader
+import { Carousel } from "react-responsive-carousel"
 import { useDocument } from "../../hooks/documents"
 import { useParams } from "react-router-dom"
 
 export default function DettaglioDocumento() {
   const { id } = useParams()
   const [{ document }] = useDocument(id)
+
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  const prev = () => {
+    setCurrentSlide(currentSlide-1)
+  }
+
+  const next = () => {
+    setCurrentSlide(currentSlide+1)
+  }
 
   console.log(document)
 
@@ -33,10 +45,32 @@ export default function DettaglioDocumento() {
                       <img src={"/zoom-in.svg"} alt="Zoom in" />
                     </div>
                   </div>
+                  <div className='d-flex align-items-center'>
+                    <div onClick={() => prev()}><img src='/arrow-left.svg' alt='back' /></div>
+                    <div className='ml-2'>{currentSlide} / {document.images.length}</div>
+                    <div className='ml-2' onClick={() => next()}><img src='/arrow-right.svg' alt='back' /></div>
+                  </div>
                 </div>
-                <div className="img-document d-flex justify-content-center mt-4 mb-4 overflow-scroll">
+                <div className="img-document d-flex justify-content-center mt-4 mb-4">
                   <TransformComponent>
-                    <img src={document.images[0].image} alt={document.spettacolo} style={{ height: '80vh'}} />
+                    <Carousel
+                      showArrows={false}
+                      showStatus={false}
+                      showIndicators={false}
+                      showThumbs={false}
+                      selectedItem={currentSlide}
+                    >
+                      {document.images &&
+                        document.images.map((image, key) => (
+                          <div key={key}>
+                            <img
+                              src={image.image}
+                              alt={document.spettacolo}
+                              style={{ width: "60%" }}
+                            />
+                          </div>
+                        ))}
+                    </Carousel>
                   </TransformComponent>
                 </div>
               </div>
