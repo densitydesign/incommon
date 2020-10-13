3f94a3cb14f6adfe07875ee42e50811b44ccf55c
skaffo <matteo.scaffidi@gmail.com>
2020-10-13 11:13:24 +0200

    Insert icons page catalogue

--- src/pages/Catalogo/Catalogo.js
+++ src/pages/Catalogo/Catalogo.js
@@ -74,16 +74,17 @@ export default function Catalogo() {
           <div className="">
             <div className="d-flex">
               <div
-                className="raggruppa-button pointer w-50"
+                className="raggruppa-button pointer w-55"
                 onClick={() => toggleCollapseDocuments()}
               >
                 {isCollapsed ? "separa i fascicoli" : "raggruppa i fascicoli"}
+                <img height='12' className='ml-2' src='raggruppa_fascicoli.svg' alt='Raggruppa fascicoli' />
               </div>
               <div
                 onClick={() => reset()}
-                className="reset-filtri w-50 pointer"
+                className="reset-filtri w-45 pointer"
               >
-                cancella i filtri
+                 cancella i filtri <img height='13' className='ml-2' src='mostra_filtri.svg' alt='Reset filtri' />
               </div>
             </div>
             <div className="count-documents">
@@ -110,20 +111,21 @@ export default function Catalogo() {
           />
         </div>
         <div className="block-catalogo  overflow-scroll ml-4 mr-4 d-flex flex-row flex-wrap">
-          <React.Suspense fallback={<>Loading users...</>}>
-            <Waypoint topOffset={'50%'} onLeave={() => setQueryParams({ page: [1,2] })}>
-              <div>
-                {documents &&
-                  documents.map((document, index) => (
-                    <DocumentCatalogItem
-                      isCollapsed={isCollapsed}
-                      key={index}
-                      document={document}
-                    />
-                  ))}
-              </div>
-            </Waypoint>
-          </React.Suspense>
+          <Waypoint
+            topOffset={"50%"}
+            onLeave={() => setQueryParams({ page: [1, 2] })}
+          >
+            <div>
+              {documents &&
+                documents.map((document, index) => (
+                  <DocumentCatalogItem
+                    isCollapsed={isCollapsed}
+                    key={index}
+                    document={document}
+                  />
+                ))}
+            </div>
+          </Waypoint>
         </div>
       </div>
     </div>
