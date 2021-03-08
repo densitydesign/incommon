9a83f50c5f391bc4b2d06d8ec95118b5286e1dcb
skaffo <matteo.scaffidi@gmail.com>
2021-03-08 14:22:57 +0100

    isnert new slideshow

--- src/pages/Slideshow/allSlideshows.js
+++ src/pages/Slideshow/allSlideshows.js
@@ -1,8 +1,9 @@
 import slideshow from './slideshow.json'
+import edoardo2 from './edoardo-2.json'
 
 export default {
 
   'luci-della-citta': slideshow,
-  'edoardo-2': slideshow,
+  'edoardo-2': edoardo2,
 
 }
