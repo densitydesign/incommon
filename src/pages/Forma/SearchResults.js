949a8983778e7c9c2fa840082574b3b3a416fa06
skaffo <matteo.scaffidi@gmail.com>
2021-03-18 17:06:30 +0100

    Styled search forma

--- src/pages/Forma/SearchResults.js
+++ src/pages/Forma/SearchResults.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Search } from "react-bootstrap-icons"
 
 export default function SearchResults({
   search,
@@ -8,25 +9,30 @@ export default function SearchResults({
 }) {
   return (
     <div>
-      <input
-        className='input-forma'
-        type="text"
-        placeholder="Cerca un nodo"
-        value={search}
-        onChange={(e) => onTextChange(e.target.value)}
-      />
-      <div>
-        <ul className='list-search-items'>
-          {searchResults.map((result, i) => (
-            <li
-              className='pointer'
-              onClick={() => onSelect(result)}
-              key={`${result.type}-${result.title}`}
-            >
-              {result.title}
-            </li>
-          ))}
-        </ul>
+      <div className="d-flex align-items-center">
+        <Search width={20} />
+        <input
+          className="input-forma ml-2"
+          type="text"
+          placeholder="Cerca un nodo"
+          value={search}
+          onChange={(e) => onTextChange(e.target.value)}
+        />
+      </div>
+      <div className="ml-4 w-100">
+        <div>
+          <ul className="list-search-items">
+            {searchResults.map((result, i) => (
+              <li
+                className="pointer"
+                onClick={() => onSelect(result)}
+                key={`${result.type}-${result.title}`}
+              >
+                {result.title}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   )
