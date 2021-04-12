9275ad570ad0ae1395d9b1084088d3a7eed40cb3
gabrielecolombo <gabrielecolombo@users.noreply.github.com>
2021-04-12 19:07:43 +0200

    slideshow nostra signora vers 1

--- src/pages/Slideshow/allSlideshows.js
+++ src/pages/Slideshow/allSlideshows.js
@@ -1,9 +1,12 @@
 import slideshow from './slideshow.json'
 import edoardo2 from './edoardo-2.json'
+import nostrasignora from './nostrasignora.json'
 
 export default {
 
   'luci-della-citta': slideshow,
   'edoardo-2': edoardo2,
+  'nostra-signora-dei-turchi': nostrasignora,
+
 
 }
