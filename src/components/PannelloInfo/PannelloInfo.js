44efa59ce16b00256d755f6e6c92fed117eb8d21
skaffo <matteo.scaffidi@gmail.com>
2021-12-10 16:04:56 +0100

    Working on panel info catalogue

--- src/components/PannelloInfo/PannelloInfo.js
+++ src/components/PannelloInfo/PannelloInfo.js
@@ -1,4 +1,5 @@
 import React from "react"
+import CataloguePanel from "./components/CataloguePanel"
 import PerformancePanel from "./components/PerformancePanel"
 import RecompositionPanel from "./components/RecompositionPanel"
 import ShapePanel from "./components/ShapePanel"
@@ -12,19 +13,7 @@ export default function PannelloInfo({ type }) {
         <TimesPanel />
       )}
       {type === "catalogo" && (
-        <div>
-          <span className={"title-panel"}>Catalogue</span> displays documents in
-          a grid layout that can be filtered according to different criteria.
-          The catalogue maintains the traditional conception of materials as
-          documents, but the filtrable grid offers the needed ‘contextual
-          pressure’01 to stimulate new readings of documents as collections. The
-          view exploits the grid as a format for collection: when displayed next
-          to each other, documents become part of a collection, which the user
-          can modify by applying various filters, prompting new visual
-          sequences. Images can be visualised as single items or folded into
-          binders when they belong to the same documents. By clicking on one
-          item, the document card opens up, showing the associated metadata.
-        </div>
+        <CataloguePanel />
       )}
       {type === "archivio" && <PerformancePanel />}
       {type === "spettacoli" && <RecompositionPanel />}
---
+++ src/components/PannelloInfo/components/CataloguePanel.js
@@ -0,0 +1,20 @@
+import React from "react"
+import "../PannelloInfo.css"
+
+export default function CataloguePanel() {
+  return (
+    <div>
+      <span className={"title-panel"}>Catalogue</span> displays documents in a
+      grid layout that can be filtered according to different criteria. The
+      catalogue maintains the traditional conception of materials as documents,
+      but the filtrable grid offers the needed ‘contextual pressure’01 to
+      stimulate new readings of documents as collections. The view exploits the
+      grid as a format for collection: when displayed next to each other,
+      documents become part of a collection, which the user can modify by
+      applying various filters, prompting new visual sequences. Images can be
+      visualised as single items or folded into binders when they belong to the
+      same documents. By clicking on one item, the document card opens up,
+      showing the associated metadata.
+    </div>
+  )
+}
