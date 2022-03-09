7caa0d8ff304a8ec6c78485f7b715ec5310098fa
gabrielecolombo <gabrielecolombo@users.noreply.github.com>
2022-03-09 17:39:18 +0100

    festival becket slideshow

--- src/pages/Slideshow/allSlideshows.js
+++ src/pages/Slideshow/allSlideshows.js
@@ -1,11 +1,14 @@
 import slideshow from './slideshow.json'
 import edoardo2 from './edoardo-2.json'
+import festivalbeckett from './festival-di-s-beckett.json'
 import nostrasignora from './nostrasignora.json'
+
 
 export default {
 
   'luci-della-citta': slideshow,
   'edoardo-2': edoardo2,
+  'festival-di-s-beckett': festivalbeckett,
   'nostra-signora-dei-turchi': nostrasignora,
 
 
