b7b458b5b14f80ffba2224c82e76dfce8263d7a6
gabrielecolombo <gabrielecolombo@users.noreply.github.com>
2022-03-10 15:57:22 +0100

    zip slideshow

--- src/pages/Slideshow/allSlideshows.js
+++ src/pages/Slideshow/allSlideshows.js
@@ -2,6 +2,7 @@ import slideshow from './slideshow.json'
 import edoardo2 from './edoardo-2.json'
 import festivalbeckett from './festival-di-s-beckett.json'
 import nostrasignora from './nostrasignora.json'
+import zip from './zip.json'
 
 
 export default {
@@ -10,6 +11,7 @@ export default {
   'edoardo-2': edoardo2,
   'festival-di-s-beckett': festivalbeckett,
   'nostra-signora-dei-turchi': nostrasignora,
+  'zip': zip,
 
 
 }
