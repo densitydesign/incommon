2ddf2ee580e8e7640b81debb621819abcf5cb57b
gabrielecolombo <gabrielecolombo@users.noreply.github.com>
2022-03-15 17:52:45 +0100

    theatre slideshow

--- src/pages/Slideshow/allSlideshows.js
+++ src/pages/Slideshow/allSlideshows.js
@@ -16,6 +16,7 @@ import mors2 from './mors-2'
 import theatrefunctionsterminated from './theatre-functions-terminated.json'
 import theatrefunctionscritical from './theatre-functions-critical.json'
 import decentramento from './decentramento.json'
+import theatre from './theatre.json'
 
 
 export default {
@@ -37,6 +38,7 @@ export default {
   'theatre-functions-terminated': theatrefunctionsterminated,
   'theatre-functions-critical': theatrefunctionscritical,
   'decentramento': decentramento,
+  'theatre': theatre,
 
 
 }
