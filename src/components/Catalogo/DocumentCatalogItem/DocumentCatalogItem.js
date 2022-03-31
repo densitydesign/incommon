4b66cb121b9319973883285460da57e72b3b48ac
skaffo <matteo.scaffidi@gmail.com>
2022-03-31 09:44:50 +0200

    Ripristino immagini catalogo senza check

--- src/components/Catalogo/DocumentCatalogItem/DocumentCatalogItem.js
+++ src/components/Catalogo/DocumentCatalogItem/DocumentCatalogItem.js
@@ -9,29 +9,20 @@ export default function DocumentCatalogItem({ document, isCollapsed }) {
     return document.images.filter((i) => i.image.match(/.(jpg|jpeg|png|gif)$/i))
   }, [document.images])
 
-  function ImageExist(url) {
-    var img = new Image()
-    img.src = url
-    return img.height !== 0
-  }
-
   return (
     <>
-      {images.map(
-        (img, index) =>
-          ImageExist(img.preview) && (
-            <img
-              // style={{ zIndex: index, marginLeft: index === 0 ? 0 : "-90%" }}
-              className={classnames("mr-4 mt-4 pointer img-catalog")}
-              onClick={() => history.push("/catalogue/" + document.id)}
-              key={index}
-              //src={img.localPreview}
-              src={img.preview}
-              alt={document.nome}
-              height="80"
-            />
-          )
-      )}
+      {images.map((img, index) => (
+        <img
+          // style={{ zIndex: index, marginLeft: index === 0 ? 0 : "-90%" }}
+          className={classnames("mr-4 mt-4 pointer img-catalog")}
+          onClick={() => history.push("/catalogue/" + document.id)}
+          key={index}
+          //src={img.localPreview}
+          src={img.preview}
+          alt={document.nome}
+          height="80"
+        />
+      ))}
     </>
   )
 }
