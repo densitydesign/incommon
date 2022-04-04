a63cbd8c4229781cab77c8884e666524e4c8ef22
skaffo <matteo.scaffidi@gmail.com>
2022-04-04 09:56:17 +0200

    Fixing doubles eventi in forma

--- src/pages/Forma/SelectedCard.js
+++ src/pages/Forma/SelectedCard.js
@@ -1,34 +1,49 @@
-import { groupBy } from "lodash"
-import React, { useMemo } from "react"
+import { groupBy, uniqBy } from 'lodash'
+import React, { useMemo } from 'react'
 import classNames from 'classnames'
 
-export default function SelectedCard({ item, relations, onSelected, selectedNode }) {
-  const relationsByType = useMemo(() => groupBy(relations, "Relazione"), [
-    relations,
-  ])
+export default function SelectedCard({
+  item,
+  relations,
+  onSelected,
+  selectedNode,
+}) {
+  const relationsByType = useMemo(
+    () => groupBy(relations, 'Relazione'),
+    [relations]
+  )
+
+  console.log(relationsByType)
   return (
     <div className="position-relative">
       <h4>{item.title}</h4>
       {Object.keys(relationsByType).map((relationName) => (
-        <div key={relationName} className='mt-4'>
+        <div key={relationName} className="mt-4">
           <h5 className="relation-name">{relationName}</h5>
           <ul className="list-events-actors">
-            {relationsByType[relationName].map((relation, i) => (
+            {uniqBy(relationsByType[relationName],'Evento').map((relation, i) => (
               <li
-                className={classNames("d-flex justify-content-between pointer",{
-                  'ligh-selected-label-graph': selectedNode === (item.type === "attore" ? relation.Evento : relation.Attore)
-                })}
+                className={classNames(
+                  'd-flex justify-content-between pointer',
+                  {
+                    'ligh-selected-label-graph':
+                      selectedNode ===
+                      (item.type === 'attore'
+                        ? relation.Evento
+                        : relation.Attore),
+                  }
+                )}
                 key={i}
                 onClick={() => {
-                  if (item.type === "attore") {
-                    onSelected(relation, "evento")
+                  if (item.type === 'attore') {
+                    onSelected(relation, 'evento')
                   } else {
-                    onSelected(relation, "attore")
+                    onSelected(relation, 'attore')
                   }
                 }}
               >
                 <span className="actor-event-item">
-                  {item.type === "attore" ? relation.Evento : relation.Attore}{" "}
+                  {item.type === 'attore' ? relation.Evento : relation.Attore}{' '}
                 </span>
                 <span className="year-item">{relation.Anno}</span>
               </li>
