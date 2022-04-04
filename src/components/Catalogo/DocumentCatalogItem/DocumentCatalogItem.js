57149916b1640f5889c3ad859199d95fa89f3968
skaffo <matteo.scaffidi@gmail.com>
2022-04-04 11:03:42 +0200

    Working on grouping documents

--- src/components/Catalogo/DocumentCatalogItem/DocumentCatalogItem.js
+++ src/components/Catalogo/DocumentCatalogItem/DocumentCatalogItem.js
@@ -1,6 +1,6 @@
-import React, { Fragment, useMemo } from "react"
-import classnames from "classnames"
-import { useHistory } from "react-router-dom"
+import React, { Fragment, useMemo } from 'react'
+import classnames from 'classnames'
+import { useHistory } from 'react-router-dom'
 
 export default function DocumentCatalogItem({ document, isCollapsed }) {
   const history = useHistory()
@@ -11,18 +11,35 @@ export default function DocumentCatalogItem({ document, isCollapsed }) {
 
   return (
     <>
-      {images.map((img, index) => (
-        <img
-          // style={{ zIndex: index, marginLeft: index === 0 ? 0 : "-90%" }}
-          className={classnames("mr-4 mt-4 pointer img-catalog")}
-          onClick={() => history.push("/catalogue/" + document.id)}
-          key={index}
-          //src={img.localPreview}
-          src={img.preview}
-          alt={document.nome}
-          height="80"
-        />
-      ))}
+      {!isCollapsed ? (
+        images.map((img, index) => (
+          <img
+            // style={{ zIndex: index, marginLeft: index === 0 ? 0 : "-90%" }}
+            className={classnames('mr-4 mt-4 pointer img-catalog')}
+            onClick={() => history.push('/catalogue/' + document.id)}
+            key={index}
+            //src={img.localPreview}
+            src={img.preview}
+            alt={document.nome}
+            height="80"
+          />
+        ))
+      ) : (
+        <div className='mr-4 mt-4'>
+          {images.map((img, index) => (
+            <img
+              style={{ zIndex: index, marginLeft: index === 0 ? 0 : '-40px' }}
+              className={classnames('pointer img-catalog')}
+              onClick={() => history.push('/catalogue/' + document.id)}
+              key={index}
+              //src={img.localPreview}
+              src={img.preview}
+              alt={document.nome}
+              height="80"
+            />
+          ))}
+        </div>
+      )}
     </>
   )
 }
