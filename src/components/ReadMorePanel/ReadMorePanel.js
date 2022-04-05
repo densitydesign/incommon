c7e300df815c0a11535415e37acd1818c87c03ae
skaffo <matteo.scaffidi@gmail.com>
2022-04-05 15:21:02 +0200

    Working on read more slideshow

--- src/components/ReadMorePanel/ReadMorePanel.js
+++ src/components/ReadMorePanel/ReadMorePanel.js
@@ -4,21 +4,29 @@ import './ReadMorePanel.css'
 export default function ReadMorePanel({ caseStudy, setReadMore }) {
   return (
     <div className="ReadMorePanel">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <h1>{caseStudy.titolo}</h1>
-            <div className="mt-3">{caseStudy.description_en}</div>
-            <div className="mt-5">{caseStudy.description}</div>
+      <div className="d-flex">
+        <div style={{ width: '50%', paddingTop: 62, paddingBottom: 62 }}>
+          <h1 style={{ paddingLeft: 110 }}>{caseStudy.titolo}</h1>
+          <div className="mt-3 d-flex" style={{ paddingLeft: 20 }}>
+            <div>EN</div>
+            <div style={{ marginLeft: 57, marginRight: 57 }}>
+              {caseStudy.description_en}
+            </div>
           </div>
-          <div className="col-md-6 pl-4">
-            <div
-              className="img-read-more"
-              style={{
-                backgroundImage: `${caseStudy.backgroundImage}`,
-              }}
-            ></div>
+          <div className="mt-5 d-flex" style={{ paddingLeft: 20 }}>
+            <div>IT</div>
+            <div style={{ marginLeft: 57, marginRight: 57 }}>
+              {caseStudy.description}
+            </div>
           </div>
+        </div>
+        <div
+          className='background-image-read-more'
+          style={{
+            backgroundImage: `${caseStudy.backgroundImage}`,
+          }}
+        >
+          {/* <div className="img-read-more"></div> */}
         </div>
       </div>
       <div className="close-read-more" onClick={() => setReadMore(false)}>
